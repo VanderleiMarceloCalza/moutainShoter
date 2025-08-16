520fe32b39cbcf9583498c4bf0c94cb76bc738ba
vandermcalza@outlook.com <Vandermcalza@outlook.com>
2025-08-16 10:21:13 -0300

    hit box collisions Hud Score

--- code/Const.py
+++ code/Const.py
@@ -2,9 +2,12 @@ import pygame
 from pygame.examples.grid import WINDOW_WIDTH
 
 # C
-COLOR_ORANGE = (255, 128, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_YEllOW = (255, 255, 0)
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YEllOW = (255, 255, 0)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
+
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COPERATIVE',
@@ -13,8 +16,7 @@ MENU_OPTION = ('NEW GAME 1P',
                'EXIT')
 
 # E
-
-EVENT_ENEMY = pygame.USEREVENT +1
+EVENT_ENEMY = pygame.USEREVENT + 1
 
 ENTITY_SPEED = {
     'Level1Bg0': 0,
@@ -31,7 +33,7 @@ ENTITY_SPEED = {
     'Enemy1': 1,
     'Enemy1Shot': 4,
     'Enemy2': 1,
-    'Enemy2Shot': 2            ,
+    'Enemy2Shot': 2,
 
 }
 ENTITY_HEALTH = {
@@ -57,6 +59,52 @@ ENTITY_HEALTH = {
     'Enemy2Shot': 1,
 }
 
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    # 'Level2Bg0': 0,
+    # 'Level2Bg1': 0,
+    # 'Level2Bg2': 0,
+    # 'Level2Bg3': 0,
+    # 'Level2Bg4': 0,
+    'Player1': 1,
+    'Player1Shot': 25,
+    'Player2': 1,
+    'Player2Shot': 20,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 15,
+
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    # 'Level2Bg0': 0,
+    # 'Level2Bg1': 0,
+    # 'Level2Bg2': 0,
+    # 'Level2Bg3': 0,
+    # 'Level2Bg4': 0,
+    'Player1': 0,
+    'Player1Shot': 0,
+    'Player2': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy1Shot': 0,
+    'Enemy2': 125,
+    'Enemy2Shot': 0,
+}
 ENTITY_SHOT_DELAY = {
     'Player1': 20,
     'Player2': 15,
@@ -67,13 +115,13 @@ ENTITY_SHOT_DELAY = {
 PLAYER_KEY_UP = {'Player1': pygame.K_w,
                  'Player2': pygame.K_UP}
 PLAYER_KEY_DOWN = {'Player1': pygame.K_s,
-                 'Player2': pygame.K_DOWN}
+                   'Player2': pygame.K_DOWN}
 PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
-                 'Player2': pygame.K_LEFT}
+                   'Player2': pygame.K_LEFT}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
-                 'Player2': pygame.K_RIGHT}
+                    'Player2': pygame.K_RIGHT}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_SPACE,
-                 'Player2': pygame.K_0}
+                    'Player2': pygame.K_0}
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
--- code/level.py
+++ code/level.py
@@ -10,7 +10,7 @@ from code.Player import Player
 from code.enemy import Enemy
 from pygame import Surface, Rect
 from pygame.font import Font
-from code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY
+from code.Const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, C_GREEN, C_CYAN
 from code.entity import Entity
 from code.entityFactory import EntityFactory
 from code.EntityMediator import EntityMediator
@@ -31,8 +31,8 @@ class Level:
 
     def run(self):
         pygame.mixer_music.load('./asset/Level1.mp3')
-        #pygame.mixer_music.load('.asset/{self.name}.mp3')
-        #pygame.mixer_music.load(f'.asset/{self.name}.mp3')
+        # pygame.mixer_music.load('.asset/{self.name}.mp3')
+        # pygame.mixer_music.load(f'.asset/{self.name}.mp3')
         pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()
 
@@ -45,17 +45,21 @@ class Level:
                     shoot = ent.shoot()
                     if shoot is not None:
                         self.entity_list.append(shoot)
+                if ent.name == 'Player1':
+                    self.level_text(14, f'Player1 - Health:{ent.health} | Score: {ent.score}', C_GREEN, (10, 25))
+                if ent.name == 'Player2':
+                    self.level_text(14, f'Player2 - Health:{ent.health} | Score: {ent.score}', C_CYAN, (10, 45))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == EVENT_ENEMY:
-                    choice = random.choice(('Enemy1','Enemy2'))
+                    choice = random.choice(('Enemy1', 'Enemy2'))
                     self.entity_list.append(EntityFactory.get_entity(choice))
             # printed text
-            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 100 : 1f}s', COLOR_WHITE, (10, 5))
-            self.level_text(14, f'fps: {clock.get_fps(): .0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
-            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 100 : 1f}s', C_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps(): .0f}', C_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', C_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
             # Verify collisions and health
             EntityMediator.verify_collision(entity_list=self.entity_list)
